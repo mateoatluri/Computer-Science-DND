e211c4bd4d19f483600ff110971ab172c7cd291f
mateoatluri <matluri2@hwemail.com>
2025-08-27 13:31:55 -0700

    end of class august 27th

--- PugRescue/PugSaver.java
+++ PugRescue/PugSaver.java
@@ -5,24 +5,57 @@ public class PugSaver {
 
 	//Moves every dog whose breed is "Pug" in the list to the back of the list
 	public static void rescuePugs(ArrayList<Dog> list) {
-		ArrayList<Dog> notGolden = new ArrayList<Dog>();
-		ArrayList<Dog> golden = new ArrayList<Dog>();
-		
+		int goldenCounter = 0;
+
 		for (int i = 0; i < list.size(); i++) {
 			String breed = list.get(i).getBreed();
 		    if (breed.toLowerCase().contains("golden")) {
-				golden.add(list.get(i));
-			} else {
-				notGolden.add(list.get(i));
+				Dog temp = list.get(i);
+
+
+				for (int j = list.size() - goldenCounter; j > 0; j--) {
+					if (i > j) {
+						break;
+					}
+					if (!(breed.toLowerCase().contains("golden"))) {
+						list.set(i, list.get(j));
+						list.set(j, temp);
+						goldenCounter++;
+					}
+				}
+
+				if (i > (list.size() - goldenCounter)) {
+					break;
+				}
 			}
 		}
 		
-		notGolden.addAll(golden);
-		list = notGolden;
+		
+		
+		
+		
+		
+		
+		
+		//attempt 2, maybe work
+		// ArrayList<Dog> notGolden = new ArrayList<Dog>();
+		// ArrayList<Dog> golden = new ArrayList<Dog>();
+		
+		// for (int i = 0; i < list.size(); i++) {
+		// 	String breed = list.get(i).getBreed();
+		//     if (breed.toLowerCase().contains("golden")) {
+		// 		golden.add(list.get(i));
+		// 	} else {
+		// 		notGolden.add(list.get(i));
+		// 	}
+		// }
+		
+		// notGolden.addAll(golden);
+		// list = notGolden;
 
 		
 		
-		
+		//attempt 1 nah
 		// for (int i = 0; i < list.size(); i++) {
 		// 	String breed = list.get(i).getBreed();
 		// 	if (breed.toLowerCase().contains("golden")) {
--- PugRescue/PugSaverTester.java
+++ PugRescue/PugSaverTester.java
@@ -1,4 +1,10 @@
+import java.util.ArrayList;
+
+
+
 public class PugSaverTester {
     
-    
+    public static void main(String[] args) {
+        
+    }
 }
