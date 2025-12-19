259d6b3e71b1db217ffcf392f57af00ef03e83a9
mateoatluri <matluri2@hwemail.com>
2025-12-19 10:02:00 -0800

    hellooo

---
+++ SortedArrayList/SortedArrayList.java
@@ -0,0 +1,27 @@
+public class SortedArrayList<E extends Comparable<E>> extends MyArrayList<E>{
+
+	
+	@Override
+	public boolean contains(E obj) {
+		
+	}
+	
+	//May not contain more than one of the same object
+	@Override
+	public boolean add(E obj) {
+		
+	}
+	
+	@Override
+	public boolean remove(E obj) {
+		
+	}
+	
+	public E min() {
+		
+	}
+	
+	public E max() {
+		
+	}
+}
