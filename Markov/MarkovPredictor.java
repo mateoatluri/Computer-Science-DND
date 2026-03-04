8ccb7fa6df100ca8aee6688d39c29201dd766849
mateoatluri <matluri2@hwemail.com>
2026-03-04 13:29:08 -0800

    I HAVE COMMITTMENT ISSUES

--- Markov/MarkovPredictor.java
+++ Markov/MarkovPredictor.java
@@ -1,27 +1,43 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 
-public class MarkovPrediction {
+public class MarkovPredictor {
 
     // Please define your own variables and data structures
-    // 
+    //
+
+    private int size;
+
+
     public ArrayList<String[]> readData(String filePath) {
         
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                
 
+            String line;
+
+            ArrayList<String[]> newString = new ArrayList<String[]>();
+            
+            
+            
+            while ((line = br.readLine()) != null) {
+                newString.add(line.split(","));
             }
+
+            return newString;
+
         } catch (Exception e) {
             e.printStackTrace();
+            return null;
         }
     }
 
 
     // Method to predict the next state given a current state
     public String predictNextState(String currentState) {
+        HashMap<String, ArrayList<String>> map = new HashMap<String, ArrayList<String>>();
+        
 
 
     }
