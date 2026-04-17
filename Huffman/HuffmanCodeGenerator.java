338d77467cce03f70da17e12413af5365e05e773
mateoatluri <matluri2@hwemail.com>
2026-04-17 11:16:42 -0700

    wrong wrong

---
+++ Huffman/HuffmanCodeGenerator.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+
+public class HuffmanCodeGenerator {
+
+
+    private HashMap<Character, Integer> frequencies;
+
+    public HuffmanCodeGenerator(String frequencyFile) {
+        getFrequencies(frequencyFile);
+    }
+
+    public void getFrequencies(String filePath) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+			
+			int charAsInt;
+			
+            // Read until the end of the stream (-1 is returned)
+			// ArrayList<Character> chars = new ArrayList<>();
+			
+
+            while ((charAsInt = reader.read()) != -1) {
+                // Cast the integer value to a character
+                char character = (char) charAsInt;
+                //System.out.print(character);
+                int value = frequencies.get(character);
+				frequencies.put(character, value + 1);
+
+            }
+
+            frequencies.put((char) 26, 1);
+
+
+        } catch (IOException e) {
+            System.err.println("An I/O error occurred: " + e.getMessage());
+        }
+    }
+
+    public int getFrequency(char c) {
+        return frequencies.get(c);
+    }
+
+    
+    
+}
---
+++ Huffman/HuffmanCodeGeneratorTester.java
@@ -0,0 +1,10 @@
+public class HuffmanCodeGeneratorTester {
+    
+    public static void main(String[] args) {
+        HuffmanCodeGenerator newThing = new HuffmanCodeGenerator("ABCDEFG.txt");
+
+        //System.out.println(HuffmanCodeGenerator("ABCDEFG.txt"));
+
+        System.out.println(newThing.getFrequency('a'));
+    }
+}
