d91c6f497509aa2aef973ec119b6751e9039f42d
mateoatluri <matluri2@hwemail.com>
2026-05-12 12:34:46 -0700

    MR GEEEESEEEEE

---
+++ DynamicProgramming/DynamicProgramming.java
@@ -0,0 +1,48 @@
+
+public class DynamicProgramming {
+
+
+	
+    // Every day for the rest of the year, you're going to be given a choice between two jobs to do: 
+    // one that is LOW stress, and one that is HIGH stress.  Each job pays out a dollar amount; 
+    // *usually* the high stress jobs pay more.  However, after doing a high stress job, you need to 
+    // REST for a day.
+
+    // Given a list of all the payouts for all the low stress and high stress jobs, 
+    // what is the most amount of money you can get?
+    
+    // You can assume lowPayouts.length == highPayouts.length
+    public static int hiLoStress(int[] lowPayouts, int[] highPayouts) {
+
+    }
+    
+    
+	// You are partaking in a scavenger hunt!
+    // You've gotten a secret map to find many of the more difficult
+    // items, but they are only available at VERY specific times at
+    // specific places.  You have an array, times[], that lists at which
+    // MINUTE an item is available, in increasing order.
+    // Items in the ScavHunt are worth varying numbers of points.
+    // You also have an array, points[], same length as times[],
+    // that lists how many points each of the corresponding items is worth.
+    // Problem is: to get from one location to the other takes 5 minutes,
+    // so if there is an item, for example, available at time 23 and another
+    // at time 27, it's just not possible for you to make it to both: you'll
+    // have to choose!
+    // Write a method that returns the maximum POINTS you can get.
+    public static int scavHunt(int[] times, int[] points) {
+	
+	}
+    
+
+
+	/* Uses memoization to calculate the route which grants the most cookies, 
+	 * starting at [0][0], only going right or down at each point */
+	public static int dynamicCookies(int[][] cookieGrid) {
+
+	}
+    
+    
+
+
+}
